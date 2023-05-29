d221212eb71acc01bdc51fd4d231b89a64c09a74
Alcadeus <aazamthakur@gmail.com>
2023-05-29 10:33:57 +0300

    Replace with local files

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Client, Account, ID } from "appwrite";
+
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+    .setProject('646ca8cc6c7c005a7d89');               // Your project ID
+
+const account = new Account(client);
+
+// Register User
+account.create(
+    ID.unique(),
+    'me@example.com',
+    'password',
+    'Jane Doe'
+).then(response => {
+    console.log(response);
+}, error => {
+    console.log(error);
+});
+
+// Subscribe to files channel
+client.subscribe('files', response => {
+    if(response.events.includes('buckets.*.files.*.create')) {
+        // Log when a new file is uploaded
+        console.log(response.payload);
+    }
+});
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app</Text>
       <StatusBar style="auto" />
     </View>
   );
